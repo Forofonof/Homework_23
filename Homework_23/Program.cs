b77f72656de9cb41fdd53cc6cfc370f7307d2c13
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-08 18:50:46 +0300

    FIX_1.3

--- Homework_23/Program.cs
+++ Homework_23/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 8, 8, 8, 8, 30};
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 9, 9, 8, 8, 30};
         int repeatNumber = 0;
         int repetitionNumber = 0;
         int counter = 1;
@@ -23,6 +23,7 @@ internal class Program
             {
                 repeatNumber = array[i - 1];
                 repetitionNumber = counter;
+                counter = 1;
             }
         }
         Console.WriteLine($"Число {repeatNumber} повторяется больше всего раз. Количество повторений: {repetitionNumber}");
