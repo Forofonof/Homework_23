b268fc9b4a044f8a9cd30b52747c71200d3b5225
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-08 17:39:46 +0300

    FIX_1.2

--- Homework_23/Program.cs
+++ Homework_23/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 8, 8, 8, 8, 30};
         int repeatNumber = 0;
         int repetitionNumber = 0;
         int counter = 1;
